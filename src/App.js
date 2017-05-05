8d2e96c34b977eb95f358be7c6b28ece237f7789
Daniel Tea <daniel_tea@spe.sony.com>
2017-05-04 22:06:16 -0700

    switch to dumb components

--- src/App.js
+++ src/App.js
@@ -26,64 +26,48 @@ function isSearched(term) {
   };
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props;
-
-    return (
-      <form>
-        {children} <input type="text" value={value} onChange={onChange}/>
-      </form>
-    );
-  }
+const Search = ({ value, onChange, children }) => {
+  return (
+    <form>
+      {children} <input type="text" value={value} onChange={onChange}/>
+    </form>
+  );
 }
 
-class Button extends Component {
-  render() {
-    const {
-      onClick,
-      className = "",
-      children,
-    } = this.props;
+const Button = ({ onClick, className = "", children }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={className}
+      type="button"
+    >
+      {children}
+    </button>
+  );
+};
 
-    return (
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >
-        {children}
-      </button>
-    );
-  }
-}
-
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-
-    return (
-      <div>
-        { list.filter(isSearched(pattern)).map(item =>
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <Button onClick={() => onDismiss(item.objectID)}>
-                  Dismiss
-                </Button>
-              </span>
-            </div>
-          )
-        }
-      </div>
-    )
-  }
-}
+const Table = ({ list, pattern, onDismiss }) => {
+  return (
+    <div>
+      { list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <Button onClick={() => onDismiss(item.objectID)}>
+                Dismiss
+              </Button>
+            </span>
+          </div>
+        )
+      }
+    </div>
+  );
+};
 
 class App extends Component {
   constructor(props) {
