52e591849f2db2098f609ff3eab9dfcf61512ea8
Daniel Tea <daniel_tea@spe.sony.com>
2017-05-04 21:42:19 -0700

    add button, search, and table components

--- src/App.js
+++ src/App.js
@@ -26,6 +26,65 @@ function isSearched(term) {
   };
 }
 
+class Search extends Component {
+  render() {
+    const { value, onChange, children } = this.props;
+
+    return (
+      <form>
+        {children} <input type="text" value={value} onChange={onChange}/>
+      </form>
+    );
+  }
+}
+
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className = "",
+      children,
+    } = this.props;
+
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+
+    return (
+      <div>
+        { list.filter(isSearched(pattern)).map(item =>
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <Button onClick={() => onDismiss(item.objectID)}>
+                  Dismiss
+                </Button>
+              </span>
+            </div>
+          )
+        }
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -57,27 +116,17 @@ class App extends Component {
 
     return (
       <div className="App">
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}>
+          Search
+        </Search>
 
-        <form>
-          <input type="text" value={searchTerm} onChange={this.onSearchChange}/>
-        </form>
-
-        { list.filter(isSearched(searchTerm)).map(item =>
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button onClick={() => this.onDismiss(item.objectID)} type="button">
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          )
-        }
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
       </div>
     );
   }
