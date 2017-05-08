eef799b63ed3147986db4321cd95f6bb74b0048b
Daniel Tea <daniel_tea@spe.sony.com>
2017-05-07 23:47:47 -0700

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -119,8 +119,6 @@ class App extends Component {
   render() {
     const { searchTerm, result } = this.state;
 
-    if (!result) { return null; }
-
     return (
       <div className="page">
         <div className="interactions">
@@ -131,11 +129,13 @@ class App extends Component {
           </Search>
         </div>
 
-        <Table
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}
-        />
+        { result &&
+          <Table
+            list={result.hits}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}
+          />
+        }
       </div>
     );
   }
