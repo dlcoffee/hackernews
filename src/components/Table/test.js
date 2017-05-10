d9906e84ff7490e5b1d0da5e5e4ab7e56bef7536
Daniel Tea <daniel_tea@spe.sony.com>
2017-05-10 13:31:50 -0700

    add enzyme and unit test

--- src/components/Table/test.js
+++ src/components/Table/test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Table from './index';
 
 describe('Table', () => {
@@ -14,6 +15,11 @@ describe('Table', () => {
   it('renders', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Table { ...props }/>, div);
+  });
+
+  it('shows two items in the list', () => {
+    const element = shallow(<Table { ...props } />);
+    expect(element.find('.table-row').length).toBe(2);
   });
 
   test('snapshots', () => {
